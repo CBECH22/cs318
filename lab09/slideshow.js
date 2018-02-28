e07e0a89a33a5513a46c251e8ea42bf3fca838ab
Bechly <BECHLYCW@uwec.edu>
2018-02-28 11:22:01 -0600

    lab09

---
+++ lab09/slideshow.js
@@ -0,0 +1,26 @@
+// The list of image URLs
+var urls = [
+    'http://www.traveller.com.au/content/dam/images/g/u/n/q/h/0/image.related.articleLeadwide.620x349.gunpvd.png/1488330286332.png',
+    'http://images.mentalfloss.com/sites/default/files/styles/mf_image_16x9/public/57159-istock-486456250.jpg?itok=pYToxS1l&resize=1100x619',
+    'https://static1.squarespace.com/static/52969c2be4b0c5f2207b2690/540a3505e4b0bf8483554e97/540a3520e4b0bf8483554edc/1409956076275/hole11+VR7E9789.jpg?format=1500w'
+  ];
+  
+  // The index (serial number) of the current image
+  var i = 0;
+  
+  // Grab handles onto HTML elements.
+  var leftButton = document.getElementById('left');
+  var rightButton = document.getElementById('right');
+  var picture = document.getElementById('picture');
+  
+  // Retreat
+  leftButton.onclick = function() {
+    i = (i - 1 + urls.length) % urls.length;
+    picture.src = urls[i]; 
+  }
+  
+  // Advance
+  rightButton.onclick = function() {
+    i = (i + 1) % urls.length;
+    picture.src = urls[i]; 
+  }
